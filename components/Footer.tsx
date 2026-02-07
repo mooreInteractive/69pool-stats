07114a777caf83e961b5225407f35ddf0603558e
Adam Moore <amooreinteractive@gmail.com>
2026-02-07 14:39:01 -0800

    update CKPool naming to 69Pool

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,23 +6,7 @@ const Footer: React.FC = () => {
       <div>
         <div className="grid grid-flow-col gap-4">
           <a
-            href="https://x.com/MrV_777"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="hover:text-primary"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              className="fill-current"
-            >
-              <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"></path>
-            </svg>
-          </a>
-          <a
-            href="https://github.com/mrv777/ckstats"
+            href="https://github.com/mooreInteractive/69pool-stats"
             target="_blank"
             rel="noopener noreferrer"
             className="hover:text-primary"
@@ -40,9 +24,8 @@ const Footer: React.FC = () => {
         </div>
       </div>
       <div>
-        <p>Always free, always open source.</p>
         <p className="break-all">
-          Donations: bc1qz9vvexjmexe8pr2aueuz6x0v94ulkx2m2sp6lr
+          Donations: bc1qe95uyt0tlg35e4nu6mchydcfs23a3x74tl79py
         </p>
       </div>
     </footer>
